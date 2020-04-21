6b5c8ac7808a42262e0f4842aaf93eae070d7603
Jan Nowacki <jasiujanow99@gmail.com>
2020-04-21 13:24:36 +0200

    01. Init commit

---
+++ src/test/java/io/jaziu/springauthorizationserver/SpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jaziu.springauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
